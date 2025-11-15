1daa05c8d5d5c5a6b76c6bee821d09b0af3ad749
David <david11jv@gmail.com>
2025-11-15 17:25:10 +0100

    Comment out tfstate S3 bucket resource to avoid redundant creation

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -1,6 +1,7 @@
-resource "aws_s3_bucket" "tf_state" {
-  bucket = "etl-weather-terraform-state"
-}
+# Necessary to have this bucket previously created to manage tfstate
+# resource "aws_s3_bucket" "tf_state" {
+#   bucket = "etl-weather-terraform-state"
+# }
 
 resource "aws_s3_bucket" "etl-weather-bucket" {
   bucket        = "etl-weather"
